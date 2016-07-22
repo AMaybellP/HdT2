84e627236e6fd399dbd51b848a524e8af3f25f64
diegolopez96 <lop141222@uvg.edu.gt>
2016-07-22 17:53:30 -0500

    Update I_Calculadora.java

--- I_Calculadora.java
+++ I_Calculadora.java
@@ -4,4 +4,34 @@ public interface I_Calculadora
 	public int Calcular (String vector);
 	public String toString();
 	public String LeerArchivo(String direccion);
+	        // Se abre el documento 
+        String fileName = "canastadefrutas2.txt";
+
+        // Leer una linea a la vez 
+        String line = null;
+
+        try {
+            // FileReader lee el texto 
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }   
+
+            // Cerrar el texto 
+            bufferedReader.close();         
+        }
+        //Si no logra abir el archivo mandar un mensaje 
+        catch(IOException ex) {
+            System.out.println(
+                "No se pudo leer el archivo '" 
+                + fileName + "'");                  
+        
+        }
+    }
 }
